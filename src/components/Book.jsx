3b30b88a2f34e07cd5c0af9c02eacfe35f2d72fd
Timothy Mai <timothy.mai13@gmail.com>
2020-03-16 20:05:14 -0700

    created Book component

---
+++ src/components/Book.jsx
@@ -0,0 +1,22 @@
+/**
+ * ************************************
+ *
+ * @module  Book.jsx
+ * @author Timothy Mai
+ * @date 3/16/20
+ * @description displays info for a book
+ *
+ * ************************************
+ */
+
+import React from 'react';
+
+function Book() {
+  return (
+    <div>
+      book
+    </div>
+  );
+}
+
+export default Book;
