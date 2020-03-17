6b126cd02eeac4cb0b36d79feb5c111ba999f280
Timothy Mai <timothy.mai13@gmail.com>
2020-03-17 14:39:38 -0700

    added structure to CreateBookContainer

--- src/containers/CreateBookContainer.jsx
+++ src/containers/CreateBookContainer.jsx
@@ -14,7 +14,31 @@ import React from 'react';
 function CreateBookContainer() {
   return (
     <div>
-      create book container
+      <h1>Create New Book</h1>
+      <button type="button">Back</button>
+      <form>
+        <label>
+          Title:
+          <input name="title" type="text" />
+        </label>
+        <br />
+        <label>
+          Author:
+          <input name="author" type="text" />
+        </label>
+        <br />
+        <label>
+          Publisher:
+          <input name="publisher" type="text" />
+        </label>
+        <br />
+        <label>
+          Published Date:
+          <input name="publishedDate" type="text" />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
