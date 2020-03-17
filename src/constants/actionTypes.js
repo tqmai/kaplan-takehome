90805ffa1c28268ba9447b5dc32d2b7656c8fc9f
Timothy Mai <timothy.mai13@gmail.com>
2020-03-17 14:40:34 -0700

    added actionTypes file

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,19 @@
+/**
+ * ************************************
+ *
+ * @module  actionTypes.js
+ * @author Timothy Mai
+ * @date 3/17/20
+ * @description action type constants
+ *
+ * ************************************
+ */
+
+// fetch book data
+export const FETCH_BOOKS = 'FETCH_BOOKS';
+
+// add new book data
+export const ADD_BOOKS = 'ADD_BOOKS';
+
+// edit search query
+export const EDIT_SEARCH_QUERY = 'EDIT_SEARCH_QUERY';
