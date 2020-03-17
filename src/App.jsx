d89c19f1ff710b136891b2fa7ed7271fe65e654a
Timothy Mai <timothy.mai13@gmail.com>
2020-03-16 20:00:34 -0700

    added description to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+/**
+ * ************************************
+ *
+ * @module  App.jsx
+ * @author Timothy Mai
+ * @date 3/16/20
+ * @description outer container of app, appended to DOM on #root
+ *
+ * ************************************
+ */
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
