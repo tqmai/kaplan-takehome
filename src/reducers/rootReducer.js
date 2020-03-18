69e4723d13e598807dbbd771f2418101defae714
Timothy Mai <timothy.mai13@gmail.com>
2020-03-18 13:05:01 -0700

    removed unneeded comments

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,12 +4,11 @@
  * @module  rootReducer.js
  * @author Timothy Mai
  * @date 3/17/20
- * @description root reducers (for Redux)
+ * @description root reducer (for Redux)
  *
  * ************************************
  */
 
-// import * as types from '../constants/actionTypes';
 import { combineReducers } from 'redux';
 import booksReducer from './booksReducer';
 import searchReducer from './searchReducer';
