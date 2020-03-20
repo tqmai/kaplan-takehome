b934bc54ceade4d5056c4d6801d0123db339b725
Timothy Mai <timothy.mai13@gmail.com>
2020-03-19 23:30:41 -0700

    added thunk middleware to store

--- src/store.js
+++ src/store.js
@@ -9,12 +9,15 @@
  * ************************************
  */
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import reducers from './reducers/rootReducer';
 
 // if we're adding middleware later, read this article to see how to add them:
 // https://github.com/zalmoxisus/redux-devtools-extension
-const store = createStore(reducers, composeWithDevTools());
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunk),
+));
 
 export default store;
