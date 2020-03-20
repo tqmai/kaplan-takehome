c45ccadfe747475d2547f08ed25a25915ca50581
Timothy Mai <timothy.mai13@gmail.com>
2020-03-20 16:24:49 -0700

    removed unneeded comment

--- src/store.js
+++ src/store.js
@@ -14,8 +14,6 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers/rootReducer';
 
-// if we're adding middleware later, read this article to see how to add them:
-// https://github.com/zalmoxisus/redux-devtools-extension
 const store = createStore(reducers, composeWithDevTools(
   applyMiddleware(thunk),
 ));
