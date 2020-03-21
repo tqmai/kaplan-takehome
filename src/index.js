ddc958cd81af03449b8bfe9de331e5d1bc6f95ee
Timothy Mai <timothy.mai13@gmail.com>
2020-03-21 14:38:35 -0700

    converted index.css to .scss

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import './index.css';
+import './stylesheets/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
