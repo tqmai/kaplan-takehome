c7582ecd8accc45aacefce39968579e009e3227d
Timothy Mai <timothy.mai13@gmail.com>
2020-03-21 21:20:58 -0700

    added search icon to search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,12 +12,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../stylesheets/SearchBar.scss';
+import '../../public/search-black-18dp.svg';
 
 function SearchBar({ searchQuery, editSearchQuery }) {
   return (
     <div>
       <form>
-        <input className="search" type="text" placeholder="Search" value={searchQuery} onChange={editSearchQuery} />
+        <div className="search">
+          <img src="search-black-18dp.svg" alt="Search icon" />
+          <input type="text" placeholder="Search" value={searchQuery} onChange={editSearchQuery} />
+        </div>
       </form>
     </div>
   );
